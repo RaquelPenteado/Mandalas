f0209cc783abfb8c89073b747c1d8532b680b664
Raquel Penteado <raquelpenteado4@gmail.com>
2023-09-29 22:36:07 -0300

    Adicionando página de mandalas

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ items.forEach(function(li) {
     li.classList.toggle('click');
   });
 });
+
+var images = [
+  'mandalas/mandala-red-right.jpeg',
+  'mandalas/mandala-red.jpeg',
+  'mandalas/mandala-red-left.jpeg',
+  'mandalas/black-white-circle.jpeg',
+  'mandalas/black-white-flowers.jpeg',
+  'mandalas/black-white-three.jpeg',
+];
+
+var container = document.getElementById("grid-container");
+
+for (var i = 0; i < images.length; i++) {
+  var cell = document.createElement("div");
+  cell.classList.add("item");
+
+  var image = document.createElement("img");
+  image.src = images[i];
+
+  cell.appendChild(image);
+  container.appendChild(cell);
+}
